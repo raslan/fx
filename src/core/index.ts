76e4104f62a543c08674a55ba8184c22fceb67c3
raslan <24810123+raslan@users.noreply.github.com>
2026-09-15 04:26:10 +0300

    feat(core): add public barrel export for the core subpath

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,1 +1,6 @@
-export {};
+export * from './currencies';
+export * from './currency-data';
+export * from './money';
+export * from './rates';
+export * from './tokenizer';
+export * from './types';
